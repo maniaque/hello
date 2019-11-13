0821ae35934ae6010c997e5154f446e66a956f03
Leo Nikolaev <maniaque.ru@gmail.com>
2019-11-13 20:27:18 +0300

    uwsgi version

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ def do_magic():
   now = datetime.datetime.now()
   return "Hello! {0}".format(now)
 
+def application(env, start_response):
+  start_response('200 OK', [('Content-Type','text/html')])
+  return [do_magic()]
+
 if __name__ == "__main__":
   if 'REQUEST_URI' in os.environ:
     print("Content-type: text/html\n\n")
