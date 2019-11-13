6288dfa380d699a9b67e2f6d02c64c500b0edd34
Leo Nikolaev <maniaque.ru@gmail.com>
2019-11-13 20:22:00 +0300

    apache2 cgi version

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import datetime
+import os
 
 def do_magic():
   now = datetime.datetime.now()
   return "Hello! {0}".format(now)
 
 if __name__ == "__main__":
-  print("Content-type: text/html\n\n")
+  if 'REQUEST_URI' in os.environ:
+    print("Content-type: text/html\n\n")
   print(do_magic())
